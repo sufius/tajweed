371d5d6f70d8b1fed167157de039c43a52335411
Sufian Abu-Rab <s.aburab@gmail.com>
2025-08-10 19:33:54 +0200

    cleanup

---
+++ scripts/deleteProperty.cjs
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, '../public/surat-densed'); // Adjust this path as necessary
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  files.forEach((file) => {
+    const filePath = path.join(directoryPath, file);
+
+    // Process only JSON files
+    if (path.extname(file) === '.json') {
+      // if (path.extname(file) === '.json') {
+      fs.readFile(filePath, 'utf8', (readErr, data) => {
+        if (readErr) {
+          console.error(`Error reading file ${file}:`, readErr);
+          return;
+        }
+
+        try {
+          const jsonData = JSON.parse(data);
+
+          // List of properties to delete
+          const toDelete = [
+            "translation"
+          ];
+
+          jsonData.forEach((item) => {
+            toDelete.forEach((prop) => {
+              if (item.hasOwnProperty(prop)) {
+                console.log(item[prop])
+                delete item[prop];
+              }
+            });
+          });
+
+
+          // Write the updated JSON back to the file
+          fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), 'utf8', (writeErr) => {
+            if (writeErr) {
+               console.error(`Error writing file ${file}:`, writeErr);
+             } else {
+               console.log(`Updated file: ${filePath}`);
+             }
+          });
+
+
+        } catch (parseErr) {
+          console.error(`Error parsing JSON in file ${file}:`, parseErr);
+        }
+      });
+    }
+  });
+});
